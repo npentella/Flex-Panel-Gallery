09540a0399e234103f744c664cc06a50043673cd
npentella <npentella@gmail.com>
2017-01-17 15:21:07 -0600

    Add JS to toggle panels open when clicked

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-console.log("linked!")
+const panels = document.querySelectorAll(".panel");
+
+function toggleOpen() {
+	this.classList.toggle('open')
+}
+
+panels.forEach(panel => panel.addEventListener("click", toggleOpen))
+panels.forEach(panel => panel.addEventListener("transitionend", handleTransitionend))
